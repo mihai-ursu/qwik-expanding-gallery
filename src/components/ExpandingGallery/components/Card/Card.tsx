4288d95c2284a0334a9f6dcdf843e4b948d5989e
Mihai Ursu <ursu.mihai.mail@gmail.com>
2023-06-12 15:37:29 +0300

    Added custom easing

--- src/components/ExpandingGallery/components/Card/Card.tsx
+++ src/components/ExpandingGallery/components/Card/Card.tsx
@@ -14,22 +14,22 @@ const Card = component$((props: CardProps) => {
     if (isServer) return; // Server guard
 
     animate(
-      `.animation-target-${index}`,
+      `.slide-${index}`,
       {
         width: isActive.value ? "100%" : `${100 / numberOfSlides}%`,
       },
       {
         duration: 0.6,
-        easing: "ease-in-out",
+        easing: [0.43, 0.13, 0.23, 0.96],
       }
     );
   });
 
   return (
     <div
-      class={combineClasses(styles.wrapper, `animation-target-${index}`)}
-      onMouseEnter$={() => (isActive.value = !isActive.value)}
-      onMouseLeave$={() => (isActive.value = !isActive.value)}
+      class={combineClasses(styles.wrapper, `slide-${index}`)}
+      onMouseEnter$={() => (isActive.value = true)}
+      onMouseLeave$={() => (isActive.value = false)}
       style={{ width: `${100 / numberOfSlides}%` }}
     >
       {/* <p>{movieName}</p> */}
